12ffcd14f8ff6ef6d7274db7bd18f4ccd344f892
Ritwick Verma <verma1997@gmail.com>
2019-10-27 16:41:06 -0700

    Fixed a logical error

--- mm.sh
+++ mm.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 ACTIVE_WINDOW_ID=$( xdotool getactivewindow )
 
-if [ "$1" == 'up' ]
-then
+if [ "$1" == 'up' ]; then
     xwininfo -wm -id $ACTIVE_WINDOW_ID | awk '/Window state:/{getline; print $1}' | grep -q 'Focused' && wmctrl -i -r $ACTIVE_WINDOW_ID -b add,maximized_vert,maximized_horz
 
     #xwininfo -wm -id $LAST_MINIMIZED_WINDOW_ID | awk '/Window state:/{getline; print $1}' | grep -q 'Hidden' && wmctrl -a $( xdotool getwindowname $LAST_MINIMIZED_WINDOW_ID)
 fi
 
-if [ "$1" == 'down' ]
-then
-    xwininfo -wm -id $ACTIVE_WINDOW_ID | awk '/Window state:/{getline; print $1}' | grep -q 'Maximized' && wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
-    
+if [ "$1" == 'down' ]; then
     xwininfo -wm -id $ACTIVE_WINDOW_ID | awk '/Window state:/{getline; print $1}' | grep -q 'Focused' && xdotool windowminimize $ACTIVE_WINDOW_ID
-
+    
+    xwininfo -wm -id $ACTIVE_WINDOW_ID | awk '/Window state:/{getline; print $1}' | grep -q 'Maximized' &&  wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
 fi
